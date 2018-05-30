887b9f220f9dfc9fe76bf7f4f526b397835cca7d
Eliran Maimon and Reese Glasscock <reese@>
2018-05-30 10:35:04 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("button#catbutt").click(function() {
+    $("ul#cats").append("<li>Meow</li>");
+    $("ul#dogs").append("<li>Woof Woof!</li>");
+  });
+
+  $("button#dogbutt").click(function() {
+    $("ul#cats").prepend("<li>Mow mow??</li>");
+    $("ul#dogs").prepend("<li>Bork bark?</li>");
+  });
+
+  $("button#catpic").click(function() {
+    $("ul#cats").append("<img src='img/kitty.jpg'>");
+
+    $("ul#cats").children("img").click(function() {
+        $(this).remove();
+    });
+  });
+
+
+
+
+
+});
